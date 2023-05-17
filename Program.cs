4289d274566a86980259e5403ec7f846f6890d28
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-05-16 21:49:24 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("digite o valor do produto");
+decimal valorproduto = Convert.ToDecimal(Console.ReadLine());
+
+
+Console.WriteLine("digite o valor a ser pago");
+decimal valorpago = Convert.ToDecimal(Console.ReadLine());
+decimal troco = (valorpago - valorproduto);
+
+if (valorpago < valorproduto)
+{
+Console.WriteLine("O valor que será pago é menor que o valor do produto.");
+}
+else
+{
+Console.WriteLine($"O valor do troco é {troco}.");
+}
